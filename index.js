9322ce0eecb3f4abd23903712018fc490ca7c60d
Marion Pierret <marion.pierretsu@gmail.com>
2023-01-24 17:47:12 +0100

    lab done

--- index.js
+++ index.js
@@ -17,7 +17,29 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Crepes",
+      level: "Easy Peasy",
+      ingredients: ["Farine", "Beurre", "Sucre", "Oeufs", "Lait"],
+      cuisine: "French",
+      dishType: "snack",
+      image: "https://assets.afcdn.com/recipe/20211122/124598_w1000h667c1cx3176cy2107cxt1161cyt477cxb5347cyb3565.webp",
+      duration: 10,
+      creator: "Marion",
+    })
+  .then((recipe) => console.log(recipe.title))
+  .then(()=> Recipe.insertMany(data))
+  .then((data)=> {
+    data.map(e => {
+      console.log(e.title)
+    })
   })
+  .then(()=> Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}))
+  .then(()=> console.log('Rigatoni alla Genovese Updated!'))
+  .then(()=> Recipe.deleteOne({title: 'Carrot Cake'}))   
+  .then(()=> console.log('Carrot Cake removed!'))
+  .then(()=> mongoose.connection.close())
+})
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
